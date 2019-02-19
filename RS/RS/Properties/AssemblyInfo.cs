c1dddbef29c78d199d0a6ae8e034b07885e0fb27
Yiheng <HenryQuan@users.noreply.github.com>
2019-02-19 22:26:00 +1100

    add assembly info

--- RS/RS/Properties/AssemblyInfo.cs
+++ RS/RS/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Realtime Statistics for World of Warships")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RS")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Yiheng 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
